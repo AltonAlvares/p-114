4bc1413b1d23df8150d1d517c86137039b2f2efc
AltonAlvares <112641343+AltonAlvares@users.noreply.github.com>
2023-08-25 12:09:09 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function setup()
+{
+    canvas=createCanvas(640, 480);
+    canvas.position(118, 250);
+
+}
+function preload()
+{
+
+}
+function draw()
+{
+
+}
+function take_snapshot()
+{
+    save('fun_filter.png');
+}
